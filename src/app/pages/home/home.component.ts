fd01f659f289fd9527e911a139a0008bb8e9bc0a
eufanzky <eufanzky@gmail.com>
2024-06-23 14:00:48 -0400

    added filters functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -13,26 +13,38 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 export class HomeComponent {
   tasks = signal<Task[]>([
     {
-      id: Date.now(),
+      id: 1,
       title: 'Instalar Angular CLI',
       completed: false,
     },
     {
-      id: Date.now(),
+      id: 2,
       title: 'Crear proyecto',
       completed: false,
     },
     {
-      id: Date.now(),
+      id: 3,
       title: 'Crear componentes',
       completed: false,
     },
     {
-      id: Date.now(),
+      id: 4,
       title: 'Crear servicio',
       completed: false,
     },
   ]);
+  filter = signal<'all' | 'pending' | 'completed'>('all');
+  tasksByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+    if (filter === 'pending') {
+      return tasks.filter((task) => !task.completed);
+    }
+    if (filter === 'completed') {
+      return tasks.filter((task) => task.completed);
+    }
+    return tasks;
+  });
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
@@ -56,10 +68,10 @@ export class HomeComponent {
     };
     this.tasks.update((tasks) => [...tasks, newTask]);
   };
-  updateTask = (index: number) => {
+  updateTask = (id: number) => {
     this.tasks.update((tasks) => {
-      return tasks.map((task, position) => {
-        if (position === index) {
+      return tasks.map((task) => {
+        if (task.id === id) {
           return {
             ...task,
             completed: !task.completed,
@@ -69,16 +81,14 @@ export class HomeComponent {
       });
     });
   };
-  deleteTask = (index: number) => {
-    this.tasks.update((tasks) =>
-      tasks.filter((task, position) => position != index)
-    );
-  };
+  deleteTask(id: number) {
+    this.tasks.update((tasks) => tasks.filter((task) => task.id !== id));
+  }
 
-  updateTaskEditingMode = (index: number) => {
-    this.tasks.update(prevState => {
-      return prevState.map((task, position) => {
-        if (position === index) {
+  updateTaskEditingMode = (id: number) => {
+    this.tasks.update((prevState) => {
+      return prevState.map((task) => {
+        if (task.id === id) {
           return {
             ...task,
             editing: true,
@@ -90,21 +100,25 @@ export class HomeComponent {
         };
       });
     });
-  }
+  };
 
-  updateTaskText = (index: number, event: Event) => {
+  updateTaskText = (id: number, event: Event) => {
     const input = event.target as HTMLInputElement;
-    this.tasks.update(prevState => {
-      return prevState.map((task, position) => {
-        if (position === index) {
+    this.tasks.update((prevState) => {
+      return prevState.map((task) => {
+        if (task.id === id) {
           return {
             ...task,
             title: input.value,
             editing: false,
           };
         }
-        return task
+        return task;
       });
     });
+  };
+
+  changeFilter(filter: 'all' | 'pending' | 'completed') {
+    this.filter.set(filter);
   }
 }
